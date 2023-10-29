331638cd6122ec88451ac8884993fdf1b6e39285
V-Sanjiv07 <vinodkumar.sanjiv2021@vitstudent.ac.in>
2023-10-29 09:12:35 +0000

    sry bruh

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 print("Hello world! PLease help me with this 403 error")
-print("this is added via main, #conflict bruh")
 print("this is a new feature, now an extra")
+print("this is added via main, #conflict bruh")
 
